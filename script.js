c294a861e2fb8e839fca2a3f05b33b1f5665f81a
deisy321 <146828736+deisy321@users.noreply.github.com>
2023-12-12 16:36:01 +0000

    melhorias

--- script.js
+++ script.js
@@ -2,64 +2,138 @@ const questions = [
     {
         question: "1ª Quais são os passos básicos para instalar um sistema operacional em um computador?",
         answers: [
-            { text: " a)Insira o disco de instalação e siga as instruções na tela.", correct: false },
+            { text: " a)Insira o disco de instalação e siga as instruções na tela.", correct: true },
             { text: "b) Baixe o instalador online e execute-o.", correct: false },
-            { text: "c) Utilize um programa de virtualização para criar uma máquina virtual.", correct: true },
+            { text: "c) Utilize um programa de virtualização para criar uma máquina virtual.", correct:false},
             { text: "d) Atualize automaticamente a partir das configurações do sistema.", correct: false }
         ]
     },
     
     {
-        question: "",
+        question: "2º Qual é a importância de conhecer as exigências do sistema operativo antes da instalação?",
         answers: [
-            { text: " a).", correct: false },
-            { text: "b) ", correct: false },
-            { text: "c)", correct: true },
-            { text: "d)", correct: false }
+            { text: " a)Apenas por curiosidade.", correct: false },
+            { text: "b)Para garantir que o sistema operativo funcione corretamente no hardware disponivel  ", correct: true  },
+            { text: "c)Para aumentar a velocidade da instalação.", correct:false },
+            { text: "d)As exigências do sistema não são relevantes.", correct: false }
+        ]
+    },
+    {
+        question: "3º Como é possível instalar um sistema operativo a partir de um dispositivo externo, como uma unidade USB?",
+        answers: [
+            { text: " a) Não é possível instalar a partir de dispositivos externos.", correct: false },
+            { text: "b) Insira o dispositivo externo e o sistema operativo será instalado automaticamente. ", correct: false },
+            { text: "c) Configure a ordem de inicialização no BIOS para iniciar a partir do dispositivo externo.", correct: true },
+            { text: "d)Desconecte todos os periféricos antes de iniciar a instalação.", correct: false }
+        ]
+    },
+    {
+        question: "4º Por que é importante selecionar opções durante o processo de instalação do sistema operativo?",
+        answers: [
+            { text: " a)Para tornar a instalação mais demorada.", correct: false },
+            { text: "b) As opções de instalação não têm impacto no sistema. ", correct: false },
+            { text: "c) Para personalizar a instalação de acordo com as necessidades do usuário.", correct:true  },
+            { text: "d)A seleção de opções só é necessária para programadores.", correct: false }
+        ]
+    },
+    {
+        question: "5º O que envolve a definição de um plano de configuração de periféricos em um sistema operativo?",
+        answers: [
+            { text: " a) Ignorar completamente a configuração dos periféricos.", correct: false },
+            { text: "b) Identificar os periféricos necessários e configurar seus drivers.", correct:true },
+            { text: "c)Delegar a configuração para o usuário final.", correct: false },
+            { text: "d) A configuração de periféricos não é necessária.", correct: false }
+        ]
+    },
+    {
+        question: "6º Por que é importante definir um modelo de logins em um sistema operativo?",
+        answers: [
+            { text: " a)Não é importante; qualquer login serve.", correct: false },
+            { text: "b)Para complicar o acesso dos usuários.", correct: false },
+            { text: "c)Para garantir a segurança e a identificação única de cada usuário.", correct: true },
+            { text: "d) Modelos de login são exclusivos para sistemas operacionais de servidor.", correct: false }
+        ]
+    },
+
+    {
+        question: "7º Como podemos personalizar a configuração do sistema operativo para atender às necessidades específicas de cada usuário?",
+        answers: [
+            { text: " a)  Não é possível personalizar a configuração.", correct: false },
+            { text: "b)Crie um único perfil para todos os usuários. ", correct: false },
+            { text: "c) Utilize as opções de personalização durante a instalação.", correct: true },
+            { text: "d)A configuração do sistema não afeta as necessidades dos usuários.", correct: false }
+        ]
+    },
+    {
+        question: "8º Por que pode ser necessário realizar manutenção do sistema operativo através da alteração da configuração do equipamento?",
+        answers: [
+            { text: " a) A manutenção não é necessária.", correct: false },
+            { text: "b)Para tornar o sistema operativo mais lento. ", correct: false },
+            { text: "c)Para adaptar o sistema às mudanças de hardware.", correct: true },
+            { text: "d)A configuração do equipamento não afeta a manutenção do sistema operativo.", correct: false }
         ]
     },
 ];
-
 let currentQuestionIndex = 0;
 
-const questionText = document.getElementById('question-text');
-const answerButtons = document.querySelectorAll('.answer-btn');
-const nextButton = document.getElementById('next-btn');
+function showQuestion() {
+    const questionText = document.getElementById('question-text');
+    const answerButtons = document.querySelectorAll('.answer-btn');
 
-startQuiz();
+    questionText.innerText = questions[currentQuestionIndex].question;
 
-function startQuiz() {
-    showQuestion(questions[currentQuestionIndex]);
-}
-
-function showQuestion(question) {
-    questionText.innerText = question.question;
-
-    question.answers.forEach((answer, index) => {
+    questions[currentQuestionIndex].answers.forEach((answer, index) => {
         answerButtons[index].innerText = answer.text;
-        answerButtons[index].onclick = () => checkAnswer(answer);
+        answerButtons[index].classList.remove('correct', 'incorrect');
+        answerButtons[index].disabled = false;
+        answerButtons[index].onclick = () => checkAnswer(answer, answerButtons[index]);
     });
 
-    nextButton.classList.add('hide');
+    document.getElementById('next-btn').classList.add('hide');
 }
 
-function checkAnswer(selectedAnswer) {
-    if (selectedAnswer.correct) {
-        // Resposta correta, faça algo se desejar
-    }
+function checkAnswer(selectedAnswer, clickedButton) {
+    const buttons = document.querySelectorAll('.answer-btn');
+
+    buttons.forEach(button => {
+        button.disabled = true;
+
+        if (button === clickedButton) {
+            if (selectedAnswer.correct) {
+                button.classList.add('correct');
+            } else {
+                button.classList.add('incorrect');
+            }
+        } else if (button.classList.contains('correct')) {
+            button.classList.add('correct');
+        }
+    });
 
     currentQuestionIndex++;
 
     if (currentQuestionIndex < questions.length) {
-        showQuestion(questions[currentQuestionIndex]);
+        setTimeout(() => {
+            buttons.forEach(button => {
+                button.classList.remove('correct', 'incorrect');
+                button.disabled = false;
+            });
+            showQuestion();
+        }, 1000);
     } else {
-        // Quiz concluído, faça algo se desejar
-        nextButton.innerText = 'Reiniciar Quiz';
-        nextButton.onclick = () => {
+        document.getElementById('next-btn').innerText = 'Reiniciar Quiz';
+        document.getElementById('next-btn').onclick = () => {
             currentQuestionIndex = 0;
-            startQuiz();
+            showQuestion();
         };
     }
 
-    nextButton.classList.remove('hide');
+    document.getElementById('next-btn').classList.remove('hide');
 }
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    showQuestion();
+}
+
+// Inicializa o quiz ao carregar a página
+window.onload = startQuiz;
